313743a63cb9907f95f2893fd3e85517b0d5dd2b
Nick <nick_airey@yahoo.com>
2018-04-26 21:24:20 +1000

    event mapping and better error handling

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ let util = require('util');
 let auth = process.env.auth;
 
 function getEvents(start, end) {
+    let paramsError = null;
+    if (!start) {
+        paramsError = '"start" parameter not found'
+    }
+    if (!end) {
+        paramsError += (paramsError) ? ', ' : '';
+        paramsError += '"end" parameter not found'
+    }
+    if (paramsError) {
+        return Promise.reject({statusCode: 500, body: paramsError});
+    }
+
     return new Promise( (resolve, reject) => {
         unirest.get('https://api.elvanto.com/v1/calendar/events/getAll.json')
             .auth(auth, 'x', true)
@@ -12,11 +24,38 @@ function getEvents(start, end) {
             .query({end: end})
             .end((response) => {
                 if (response.error) {
-                    reject(response);
+                    logError(response.error);
+                    reject( {statusCode: response.statusCode, body: response.statusMessage});
                 } else {
-                    resolve(response.body);
+                    switch (response.body.status) {
+                        case 'ok':
+                            resolve({statusCode: response.statusCode, body: response.body});
+                            break;
+
+                        case 'fail':
+                            //logError(response);
+                            reject({statusCode: response.body.error.code, body: response.body.error.message});
+                            break;
+
+                        default:
+                            reject({statusCode: 500, body: 'unknown EL response code'})
+                    }
                 }
             });
+    });
+}
+
+function mapEvents(response) {
+    return new Promise( (resolve, reject) => {
+        try {
+            response.body.events.event.map(function (event) {
+                delete event.url;
+                return event;
+            });
+            resolve(response);
+        } catch (err) {
+            reject( {statusCode: 500, body: 'error mapping events'});
+        }
     });
 }
 
@@ -25,9 +64,18 @@ function logObject(data) {
     return data;
 }
 
+function logError(data) {
+    console.error(util.inspect(data, {showHidden:false, depth:5}));
+    return data;
+}
+
 exports.handler = async (event) => {
-    return getEvents(event.start, event.end);
+    return getEvents(event.start, event.end)
+        .then(mapEvents)
+        .then(logObject)
+        .catch((err) => {
+            return(logError(err))
+        });
 };
 
-exports.handler({start: '2018-04-01', end: '2018-05-01'}).then(logObject);
-
+exports.handler({start: '2018-04-01', end: '2018-05-01'});
