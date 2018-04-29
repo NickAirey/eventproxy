b3f916c5224d7eb48b608f8fa0e1010e9ffbfedf
EC2 Default User <ec2-user@ip-172-31-36-60.ap-southeast-1.compute.internal>
2018-04-29 01:29:47 +0000

    better promise implementation

--- eventrss.js
+++ eventrss.js
@@ -1,22 +1,16 @@
 
-
-var libxslt = require('libxslt');
+let libxslt = require('libxslt');
 let unirest = require('unirest');
 let util = require('util');
-
-var fs = require('fs');
-
-
-
+const END_DAY_OFFSET = 90;
 
 let auth = process.env.auth;
 
-function getEvents(start, end) {
+function getEvents(queryObject) {
     return new Promise( (resolve, reject) => {
-        unirest.get('https://api.elvanto.com/v1/calendar/events/getAll.xml')
+        unirest.get('https://api.elvanto.com/v1/calendar/events/getAll.xmls')
             .auth(auth, 'x', true)
-            .query({start: start})
-            .query({end: end})
+            .query(queryObject)
             .end((response) => {
                 if (response.error) {
                     logError(response.error);
@@ -28,9 +22,15 @@ function getEvents(start, end) {
     });
 }
 
-function logObject(data) {
-    console.log(util.inspect(data, {showHidden:false, depth:5}));
-    return data;
+function getStyleSheet(stylesheetName) {
+    return new Promise( (resolve, reject) => {
+        libxslt.parseFile(stylesheetName, (error, stylesheet) => {
+            if (error) {
+                reject(error);
+            }
+            resolve(stylesheet);
+        });
+    });
 }
 
 function logError(data) {
@@ -38,26 +38,53 @@ function logError(data) {
     return data;
 }
 
-
-// make this return a promise
-function styleSheet() {
-    let stylesheetString = fs.readFileSync('rss.xsl','utf8');
-    let stylesheet = libxslt.parse(stylesheetString);
-
+function logObject(data) {
+    console.log(util.inspect(data, {showHidden:false, depth:5}));
+    return data;
 }
 
-function convertObject(documentString) {
-    let stylesheetString = fs.readFileSync('rss.xsl','utf8');
-    let stylesheet = libxslt.parse(stylesheetString);
-
-    return stylesheet.apply(documentString);
-}
-
-return getEvents('2018-04-01', '2018-05-01')
-    .then(convertObject)
-    .then(logObject)
-    .catch((err) => {
-        return(logError(err));
+// takes [stylesheetObject, xmlInput]
+function applyStylesheet(args) {
+    return new Promise( (resolve, reject) => {
+        const stylesheet = args[0];
+        const xmlInput = args[1];
+        stylesheet.apply(xmlInput, null, null, (error, xmlOutput) => {
+            if (error) {
+                reject(error);
+            }
+            resolve(xmlOutput);
+        });
     });
+}
+
+function getQueryObject() {
+    let startDate = new Date();
+    startDate.setHours(0,0,0,0);
+    let startDateStr = startDate.toISOString().slice(0, 10);
+    
+    let endDate = new Date();
+    endDate.setDate(startDate.getDate()+END_DAY_OFFSET);
+    let endDateStr = endDate.toISOString().slice(0, 10);
+
+    return logObject({start: startDateStr, end: endDateStr});
+}
+
+function invoke() {
+    let queryObject = getQueryObject();
+    
+    // retrieve stylsheet and event data
+    Promise.all([getStyleSheet('rss.xsl'), getEvents(queryObject)])
+    .then(applyStylesheet)
+    .then(xmlOutput => {
+        let x = {statusCode: 200, body: xmlOutput};
+        return logObject(x);
+    })
+    .catch((err) => {
+        // TODO: read the reject object - convert upstream message
+        //let x = {statusCode: statusCode, body: err.message};
+        return logError(err);
+    });
+}
 
 
+invoke();
