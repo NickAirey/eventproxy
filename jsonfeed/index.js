a94a96a7cefcf3dae6e9aa067166f4c17ebc7dd2
EC2 Default User <nick_airey@yahoo.com>
2018-05-03 11:11:22 +0000

    add functionality

--- jsonfeed/index.js
+++ jsonfeed/index.js
@@ -1,28 +1,97 @@
-'use strict';
 
-var util=require('util');
+let unirest = require('unirest');
+let util = require('util');
 
-console.log('Loading function');
+const END_DAY_OFFSET = 30;
 
+let auth = process.env.auth;
+
+function getEvents(queryObject) {
+
+    return new Promise( (resolve, reject) => {
+        unirest.get('https://api.elvanto.com/v1/calendar/events/getAll.json')
+            .auth(auth, 'x', true)
+            .query(queryObject)
+            .end((response) => {
+                if (response.error) {
+                    reject(response.error);
+                } 
+                switch (response.body.status) {
+                    case 'ok':
+                        resolve(response.body.events);
+                        break;
+
+                    case 'fail':
+                        reject(response.body);
+                        break;
+
+                    default:
+                        reject('unknown EL response code');
+                }
+            });
+    });
+}
+
+function addTZOffsetAndConvertToISOStr(dateStr) {
+    const OFFSET_HOURS = 10;
+    var date1 = new Date(dateStr);
+    date1.setHours(date1.getHours()+OFFSET_HOURS);
+    let date1UTCStr = date1.toUTCString();
+    return date1UTCStr.substring(0, date1UTCStr.length-4)+" +"+OFFSET_HOURS+"00";
+}
+
+
+function mapEvents(response) {
+    return new Promise( (resolve, reject) => {
+        try {
+            response.event.forEach(event => {
+                delete event.url;
+                event.start_date = addTZOffsetAndConvertToISOStr(event.start_date);
+                event.end_date = addTZOffsetAndConvertToISOStr(event.end_date);
+            });
+            resolve(response);
+        } catch (err) {
+            reject( {statusCode: 500, body: 'error mapping events'});
+        }
+    });
+}
+
+function logObject(data) {
+    console.log(util.inspect(data, {showHidden:false, depth:5}));
+    return data;
+}
+
+function logError(data) {
+    console.error(util.inspect(data, {showHidden:false, depth:5}));
+    return data;
+}
+
+function getQueryObject() {
+    let startDate = new Date();
+    startDate.setHours(0,0,0,0);
+    let startDateStr = startDate.toISOString().slice(0, 10);
+    
+    let endDate = new Date();
+    endDate.setDate(startDate.getDate()+END_DAY_OFFSET);
+    let endDateStr = endDate.toISOString().slice(0, 10);
+
+    return logObject({start: startDateStr, end: endDateStr});
+}
+
+/*
+  invocation entry point
+*/
 exports.handler = function(event, context, callback) {
 
   console.log(util.inspect(event));
 
-  if (event.body) {
-    event = JSON.parse(event.body);
-  }
-
-  console.log('value1 =', event.key1);
-  console.log('value2 =', event.key2);
-  console.log('value3 =', event.key3);
-
-  const response = {
-    statusCode: 200,
-    headers: { "Content-type": "application/json" },
-    body: JSON.stringify( { "message": event.key1 } )
-  };
-
-  console.log(util.inspect(response));
-
-  callback(null, response);
+  getEvents(getQueryObject())
+    .then(mapEvents)
+    .then(jsonOutput => {
+        callback(null, logObject({statusCode: 200, body: jsonOutput}));
+    })
+    .catch(err => {
+        logError(err);
+        callback(null, logError({statusCode: 503, body: 'error processing request'}));
+    });
 };
--- rssfeed/index.js
+++ rssfeed/index.js
@@ -1,28 +1,119 @@
-'use strict';
 
-var util=require('util');
+let libxslt = require('libxslt');
+let libxmljs = libxslt.libxmljs;
+let unirest = require('unirest');
+let util = require('util');
 
-console.log('Loading function');
+const END_DAY_OFFSET = 30;
 
+let auth = process.env.auth;
+
+function getEvents(queryObject) {
+    return new Promise( (resolve, reject) => {
+        unirest.get('https://api.elvanto.com/v1/calendar/events/getAll.xml')
+            .auth(auth, 'x', true)
+            .query(queryObject)
+            .end((response) => {
+                if (response.error) {
+                    reject(response.error);
+                }
+                if (response.status >= 200 && response.status < 300) {
+                    resolve(response.body);
+                }
+                reject(response.body);
+            });
+    });
+}
+
+function getStyleSheet(stylesheetName) {
+    return new Promise( (resolve, reject) => {
+        libxslt.parseFile(stylesheetName, (error, stylesheet) => {
+            if (error) {
+                reject(error);
+            }
+            resolve(stylesheet);
+        });
+    });
+}
+
+function logError(data) {
+    console.error(util.inspect(data, {showHidden:false, depth:5}));
+    return data;
+}
+
+function logObject(data) {
+    console.log(util.inspect(data, {showHidden:false, depth:5}));
+    return data;
+}
+
+/*
+   this is a nasty hack to add 10 hours to a UTC date str
+*/
+function addTZOffsetAndConvertToISOStr(dateStr) {
+    const OFFSET_HOURS = 10;
+    var date1 = new Date(dateStr);
+    date1.setHours(date1.getHours()+OFFSET_HOURS);
+    let date1UTCStr = date1.toUTCString();
+    return date1UTCStr.substring(0, date1UTCStr.length-4)+" +"+OFFSET_HOURS+"00";
+}
+
+function addTZOffsetAndConvertToISOElement(dateElement) {
+    dateElement.text(addTZOffsetAndConvertToISOStr(dateElement.text()));
+}
+
+
+// takes [stylesheetObject, xmlInputStr]
+// preprocesses xml to convert dates to ISO format and convert from UTC to local TZ
+function applyStylesheet(args) {
+    return new Promise( (resolve, reject) => {
+        const stylesheet = args[0];
+        const xmlInputStr = args[1];
+        
+        var xmlDoc = libxmljs.parseXml(xmlInputStr, { noblanks: true });
+        
+        xmlDoc.get('//rsp').attr({ run_date: addTZOffsetAndConvertToISOStr(new Date(Date.now()).toUTCString()) });
+        xmlDoc.find('//start_date').forEach(element => addTZOffsetAndConvertToISOElement(element));
+        xmlDoc.find('//end_date').forEach(element => addTZOffsetAndConvertToISOElement(element));
+        
+        stylesheet.apply(xmlDoc.toString(), null, null, (error, xmlOutput) => {
+            if (error) {
+                reject(error);
+            }
+            resolve(xmlOutput);
+        });
+    });
+}
+
+function getQueryObject() {
+    let startDate = new Date();
+    startDate.setHours(0,0,0,0);
+    let startDateStr = startDate.toISOString().slice(0, 10);
+    
+    let endDate = new Date();
+    endDate.setDate(startDate.getDate()+END_DAY_OFFSET);
+    let endDateStr = endDate.toISOString().slice(0, 10);
+
+    return logObject({start: startDateStr, end: endDateStr});
+}
+
+/*
+  invocation entry point
+  
+  parallel: retrieve stylsheet and xml event data
+    apply stylesheet to xml
+    return result
+*/
 exports.handler = function(event, context, callback) {
 
   console.log(util.inspect(event));
 
-  if (event.body) {
-    event = JSON.parse(event.body);
-  }
-
-  console.log('value1 =', event.key1);
-  console.log('value2 =', event.key2);
-  console.log('value3 =', event.key3);
-
-  const response = {
-    statusCode: 200,
-    headers: { "Content-type": "application/json" },
-    body: JSON.stringify( { "message": event.key1 } )
-  };
-
-  console.log(util.inspect(response));
-
-  callback(null, response);
+  Promise.all([getStyleSheet('rss.xsl'), getEvents(getQueryObject())])
+    .then(applyStylesheet)
+    .then(xmlOutput => {
+        callback(null, logObject({statusCode: 200, headers: {'Content-Type': 'text/xml'}, body: xmlOutput}));
+    })
+    .catch(err => {
+        logError(err);
+        callback(null, logError({statusCode: 503, body: 'error processing request'}));
+    });
 };
