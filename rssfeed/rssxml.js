155a2fa64eb49c5e2c90a975e4ac0e28d7cbf8a5
Nick <nick_airey@yahoo.com>
2018-10-16 14:19:30 +1100

    rss xml generation code

---
+++ rssfeed/rssxml.js
@@ -0,0 +1,73 @@
+/**
+ * Author: Nick Airey
+ */
+
+
+let builder = require('xmlbuilder');
+
+Date.prototype.addHours = function(h) {
+    this.setTime(this.getTime() + (h*60*60*1000));
+    return this;
+};
+
+/**
+ * adds a specified offset hours to a date string, and returns the string in UTC format
+ *
+ * @param UTCDateStr
+ * @param feedUTCOffset
+ * @returns {string}
+ */
+function addTZOffsetISOElement(UTCDateStr, feedUTCOffset) {
+    var date = new Date(UTCDateStr+ " UTC");
+    date.addHours(feedUTCOffset);
+    return date.toUTCString().slice(0, -4)+" +"+feedUTCOffset+"00";
+}
+
+
+/**
+ * convert event object to rss object
+ *
+ * @param event
+ * @returns {{item: {title: *, description: *, pubDate: *}}}
+ */
+exports.eventToRssItem = function (event, feedUtcOffset) {
+
+    let utcDateStr = event.start_date;
+    let aestDateStr = addTZOffsetISOElement(utcDateStr, feedUtcOffset);
+
+    return {
+        item: {
+            title: event.name,
+            description: event.description,
+            pubDate: aestDateStr,
+            guid: { '@isPermaLink': false, '#text': event.id }
+        }
+    };
+}
+
+/*
+    create a rss compliant xml string, merging the rssItems, the config and run date.
+ */
+exports.rssXmlBuilder = function(events, feedConfig, runDate) {
+
+    let feedUtcOffset = feedConfig['/organisation/events/date-utc-offset'];
+
+    let root = builder.create('rss')
+        .att('version', '2.0').att('xmlns:atom', "http://www.w3.org/2005/Atom")
+        .element('channel')
+        .element('title', feedConfig["/organisation/general/name"]).up()
+        .element('description', feedConfig["/organisation/events/feed-description"]).up()
+        .element('link', feedConfig["/organisation/general/website-url"]).up()
+        .element('pubDate', addTZOffsetISOElement(runDate, feedUtcOffset)).up()
+        .element('atom:link').att('href', feedConfig["/organisation/events/feed-url"]).att('rel', 'self').att('type', 'application/rss+xml').up();
+
+    // convert each event to rss format and add to xml builder root context
+    events.event.forEach( e => {
+        root.element(exports.eventToRssItem(e, feedUtcOffset));
+    });
+
+    let xmlStr = root.end({pretty: true});
+    console.log(xmlStr);
+
+    return xmlStr;
+};
---
+++ rssfeed/test/rssxml_test.js
@@ -0,0 +1,46 @@
+
+let assert = require('assert');
+let fs = require('fs');
+let rssxml = require('../rssxml');
+
+describe('object conversion', () => {
+
+    it('item generation', () => {
+
+        let event = {
+            "id": "52139416-ff4d-11e2-847a-46e7b7dc2836",
+            "name": "Church Picnic",
+            "description": "Come down and enjoy a fun family picnic!",
+            "where": "The Park",
+            "start_date": "2018-06-03 04:00:00"
+        };
+
+        let rssItemRef = {
+            item: {
+                title: "Church Picnic",
+                description: "Come down and enjoy a fun family picnic!",
+                pubDate: "Sun, 03 Jun 2018 14:00:00 +1000",
+                guid: {
+                    '#text': "52139416-ff4d-11e2-847a-46e7b7dc2836",
+                    '@isPermaLink': false
+                }
+            }
+        };
+
+        assert.deepEqual(rssxml.eventToRssItem(event, "10"), rssItemRef);
+    });
+
+    it('full rss xml generation', () => {
+        let events = fs.readFileSync('test/testEvents.in.json', 'utf-8');
+        assert.equal(events != null, true);
+
+        let config = fs.readFileSync('test/config_test.json', 'utf-8');
+
+        let xmlGenerated = rssxml.rssXmlBuilder(JSON.parse(events).events, JSON.parse(config), new Date(2018, 4, 30, 9));
+
+        let xmlReference = fs.readFileSync('test/testEvents.out.xml', 'utf-8');
+
+        assert.deepEqual(xmlGenerated, xmlReference);
+    });
+
+});
